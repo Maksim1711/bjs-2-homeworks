1df08a635167896072cf764a5e128f0ee332a315
Maksim Oloviannikov <olovjanqnet@gmail.com>
2022-04-05 14:22:20 +0900

    added task #1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,26 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+	let arr;
+	// код для задачи №1 писать здесь
+	"use strict";
+	arr = {};
+	let discriminant = [b ** 2 - 4 * a * c];
+	if (discriminant < 0)
+		return [];
+	if (discriminant > 0) {
+		let root = [];
+		root.push((-b + Math.sqrt(discriminant)) / (2 * a));
+		root.push((-b - Math.sqrt(discriminant)) / (2 * a));
+		arr = root;
+	} else if (discriminant == 0)
+		arr = [(-b + Math.sqrt(discriminant)) / (2 * a)];
+	return arr; // array
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+	let totalAmount;
 
-  // код для задачи №2 писать здесь
+	// код для задачи №2 писать здесь
 
-  return totalAmount;
+	return totalAmount;
 }
