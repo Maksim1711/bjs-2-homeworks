ac8c3d44aa4f366c42b3fb12dfaf562fd03662d5
Maksim Oloviannikov <olovjanqnet@gmail.com>
2022-04-14 17:11:05 +0900

    added task.js (доработка#1)

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,15 +2,14 @@ function compareArrays(arr1, arr2) {
 	let result;
 
 	// Ваш код
-	compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((item) => item === arr2[item]);
-	result = (arr1.every((item) => item !== arr2[item]));
-	return result; // boolean
+	return result = arr1.every((elem, idx) => arr2[idx] === elem && arr1.length === arr2.length);
+	// boolean
 };
 
 function advancedFilter(arr) {
 	let resultArr;
 
 	// Ваш код
-	resultArr = arr.filter((item) => item > -1).filter((item) => item % 3 === 0).map((item) => item * 10);
-	return resultArr; // array
+	return resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);;
+	// array
 };
