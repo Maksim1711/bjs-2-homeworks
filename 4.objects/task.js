8f055bdcbf80bda73fe4324359c63bfe7cc81b68
Maksim Oloviannikov <olovjanqnet@gmail.com>
2022-04-25 16:23:10 +0900

    added tast.js #1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,44 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	// Ваш код
 }
+let student1 = new Student("Mark", "male", 32);
+let student2 = new Student("Masha", "female", 30);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+	//ваш код
+	this.subject = subjectName;
 }
 
+Student.prototype.addMark = function (mark) {
+	if (this.marks === undefined) {
+		this.marks = [mark];
+	}
+	else {
+		this.marks.push(mark);
+	}
+}
+
+Student.prototype.addMarks = function (...rest) {
+	if (this.marks === undefined) {
+		this.marks = [...rest];
+	}
+	else {
+		this.marks.push(...rest);
+	}
+}
+
+Student.prototype.getAverage = function () {
+	let sum = 0;
+	this.marks.forEach((mark) => { sum += parseInt(mark) });
+	return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
+}
 // ваш код для остальных методов
